46054c0220184a9dede017ecab8104232f2a6357
Nandusasikumar1 <nandusnandanam@gmail.com>
2022-06-27 19:29:22 +0530

    updated

--- ChangeTracker.py
+++ ChangeTracker.py
@@ -1,5 +1,7 @@
 from ObjectTracker import track
 import json
+from datetime import datetime
+
 
 class mydict(dict):
     count=0
@@ -7,10 +9,10 @@ class mydict(dict):
     instances={}
     
     
-    def SaveChanges(self):
+    def SaveChanges(self,name:str):
         if self.append(self.instance_tracker)!=dict(self):
-            track.TrackChanges(self)
-            track.label_instance(self)
+            track.TrackChanges(self,name)
+            # track.label_instance(self)
       
     
     def append(self,appending_list:list):
@@ -20,12 +22,35 @@ class mydict(dict):
         except:
             pass
     
-    def __call__(self,track=True):
+    @classmethod
+    def label_instance(cls):
+
+        for i in cls.instance_tracker:
+            for change_index, change in (i.items()):
+                if change_index not in cls.instances:
+                    cls.instances[change_index]=[]
+                cls.instances[change_index].append(change)
+
+        try:
+            
+            with  open('changes.json','r') as file:
+                cls.instances=cls.instances|{'created_at':str(datetime.now())} 
+                json_list=json.load(file)
+                json_list.append(cls.instances)
+                with open('changes.json','w')as file1:
+                    json.dump(json_list,file1,ensure_ascii=False,indent=2)
+
+        except:
+            with open('changes.json','w') as file:
+            # json.dump({'created_at':str(datetime.now())},file)  
+                cls.instances=cls.instances|{'created_at':str(datetime.now())} 
+                json.dump([cls.instances],file,ensure_ascii=False,indent=2)
+
+    def __call__(self,name,track=True):
 
         if track==True:
-            self.SaveChanges()
-        with open('changes.json','w') as file:   
-            json.dump(self.instances,file)
+            self.SaveChanges(name)
+           
          
     
 
--- track.py
+++ track.py
@@ -1,20 +1,7 @@
-def TrackChanges(self):
 
-        self.count+=1
+def TrackChanges(self,name):
         instance_track={}
-        instance_track[self.count]=dict(self)
+        instance_track[name]=dict(self)
         self.instance_tracker.append(instance_track )
       
-def label_instance(self):
-
-    count_instance=0
-
-    for i in self. instance_tracker:
-        for change_index, change in (i.items()):
-            if change_index==1:
-                count_instance+=1
-                self.instances['instance-'+str(count_instance)]=[]
-                self.instances['instance-'+str(count_instance)].append(change)
-            else:
-                self.instances['instance-'+str(count_instance)].append(change)
 
