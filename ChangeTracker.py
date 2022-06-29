87412fc68b28a8c3f447cf9f4d9c4de6b4b1f75d
Nandusasikumar1 <nandusnandanam@gmail.com>
2022-06-29 11:52:16 +0530

    new

--- ChangeTracker.py
+++ ChangeTracker.py
@@ -1,20 +1,17 @@
 from ObjectTracker import track
 import json
 from datetime import datetime
-
+import os
 
 class TrackerDict(dict):
     count=0
     instance_tracker=[]
     instances={}
-    
-    
+    name=None
     def SaveChanges(self,name:str):
         if self.append(self.instance_tracker)!=dict(self):
             track.TrackChanges(self,name)
-            # track.label_instance(self)
-      
-    
+          
     def append(self,appending_list:list):
         
         try:
@@ -23,35 +20,50 @@ class TrackerDict(dict):
             pass
     
     @classmethod
-    def label_instance(cls):
-
+    def label_instance(cls,overwrite=False):
+        
         for i in cls.instance_tracker:
             for change_index, change in (i.items()):
                 if change_index not in cls.instances:
                     cls.instances[change_index]=[]
                 cls.instances[change_index].append(change)
-
-        try:
-            
-            with  open('changes.json','r') as file:
-                cls.instances=cls.instances|{'created_at':str(datetime.now())} 
-                json_list=json.load(file)
-                json_list.append(cls.instances)
-                with open('changes.json','w')as file1:
-                    json.dump(json_list,file1,ensure_ascii=False,indent=2)
-
-        except:
+        
+        if overwrite==True:
             with open('changes.json','w') as file:
-            # json.dump({'created_at':str(datetime.now())},file)  
-                cls.instances=cls.instances|{'created_at':str(datetime.now())} 
+                cls.instances.update({'created_at':str(datetime.now())}) 
                 json.dump([cls.instances],file,ensure_ascii=False,indent=2)
         
+        elif overwrite==False:
+            try:
+                
+                with  open('changes.json','r') as file:
+                    cls.instances.update({'created_at':str(datetime.now())})  
+                    json_list=json.load(file)
+                    json_list.append(cls.instances)
+                    with open('changes.json','w')as file1:
+                        json.dump(json_list,file1,ensure_ascii=False,indent=2)
+
+            except:
+                with open('changes.json','w') as file:
+                    cls.instances.update({'created_at':str(datetime.now())}) 
+                    json.dump([cls.instances],file,ensure_ascii=False,indent=2)
+        else:
+            raise Exception('Only Boolean values are acceptable')
+        
         return cls.instances
+    
+    @classmethod
+    def remove_json(cls):
+        os.remove('changes.json')
+    
+    def set_name(self,name):
+        self.name=name
+        return name
 
-    def __call__(self,name,track=True):
-
+    def __call__(self,track=True):
         if track==True:
-            self.SaveChanges(name)
+            self.SaveChanges(self.name)
+           
            
          
     
